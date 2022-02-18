a14eb8f30548f265691faae8cd98da83f780d837
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-17 20:50:37 -0400

    config variables de entorno

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
+require('dotenv').config()
+
 
 
 //Crear el servidor de express
 const app = express()
 
+//directorio publico
+app.use(express.static('public'))
+
 //rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-})
+// app.get('/', (req, res) => {
+//     res.json({
+//         ok: true
+//     })
+// })
 
 
 //Escuchar peticiones
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })

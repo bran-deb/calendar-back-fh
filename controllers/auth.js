93b01917e8646693ad9d0f6133f11cc7e98a39ae
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-19 18:24:03 -0400

    express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,23 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator')
 
 
 const crearUsuario = (req, res = response) => {
 
     const { name, email, password } = req.body
 
-    if (name.length < 5) {
+    //manejo de errores
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
         return res.status(400).json({
             ok: false,
-            msg: 'el nombre debe de ser de 5 letras'
+            errors: errors.mapped()
         })
     }
+    console.log(errors)
 
-    res.json({
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -24,6 +29,16 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
 
     const { email, password } = req.body
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            error: errors.mapped()
+        })
+    }
+    console.log(errors)
 
     res.json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,28 @@
 
 
 const { Router } = require('express')
+const { check } = require('express-validator')
 const router = Router()
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
 
 
-router.post('/new', crearUsuario)
-router.post('/', loginUsuario)
+router.post(
+    '/new',
+    [//middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),  //si no hay name
+        check('email', 'El email es obligatorio').isEmail(),    //si no es email
+        check('password', 'El password debe de tener 6 caracteres').isLength({ min: 6 })
+    ],
+    crearUsuario
+)
+router.post(
+    '/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de tener 6 caracteres').isLength({ min: 6 })
+    ],
+    loginUsuario
+)
 router.get('/renew', revalidarToken)
 
 

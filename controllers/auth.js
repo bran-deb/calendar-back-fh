cc894ac637fbd0d714de1ba1a2fb83d29596f634
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-20 01:30:37 -0400

    Create user in DB
    - added schema of users
    - added req.body & save in the instance user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,24 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario')
 
+const crearUsuario = async (req, res = response) => {
+    // const { name, email, password } = req.body
+    try {
+        const usuario = new Usuario(req.body)
+        await usuario.save()                    //guarda en la DB
 
-const crearUsuario = (req, res = response) => {
-    const { name, email, password } = req.body
+        res.status(201).json({
+            ok: true,
+            msg: 'registro',
+        })
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password
-    })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el admi'
+        })
+    }
 }
 
 const loginUsuario = (req, res = response) => {

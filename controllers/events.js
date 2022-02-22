a670c8421fce5e4aca6f931f27606d645233d0a7
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-22 12:39:15 -0400

    guardar el evento en la DB

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express")
+const Evento = require("../models/Evento")
 
 
 
@@ -9,12 +10,27 @@ const getEventos = (req, res = response) => {
     })
 }
 
-const crearEvento = (req, res = response) => {
+const crearEvento = async (req, res = response) => {
+    //crea un nuevo evento en la DB
+    const evento = new Evento(req.body)
 
-    res.status(200).json({
-        ok: true,
-        msg: 'crearEvento'
-    })
+    try {
+        evento.user = req.uid
+
+        const eventoGuardado = await evento.save()
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el admi'
+        })
+    }
 }
 
 const actualizarEvento = (req, res = response) => {

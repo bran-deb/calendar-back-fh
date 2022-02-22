e57d460eb7f99ceca11c5e2c3fe5d781c4a2f6a6
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-22 12:38:35 -0400

    update events & custom object

--- models/Evento.js
+++ models/Evento.js
@@ -5,25 +5,34 @@ const { model, Schema } = require('mongoose')
 const EventoSchema = Schema({
     title: {
         type: String,
-        require: true
+        required: true
     },
     notes: {
         type: String
     },
     start: {
         type: Date,
-        require: true
+        required: true
     },
     end: {
         type: Date,
-        require: true
+        required: true
     },
     user: {                                 //usuario que creo el registro
         type: Schema.Types.ObjectId,        //referencia
-        ref: 'Usuario'
+        ref: 'Usuario',
+        required: true
     }
 })
 
+
+//sobreEscribir toJson
+EventoSchema.method('toJSON', function () {
+    //tiene las propiedades del json y destructura _v, _id y una copia del object
+    const { __v, _id, ...object } = this.toObject()
+    object.id = _id     //cambia la propiedad de _id a id
+    return object       //retorna el object sin _v y con id
+})
 
 
 module.exports = model('Evento', EventoSchema)

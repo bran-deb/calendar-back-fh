048f989cc35e5adca83719a438931c4268a17475
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-02-21 23:50:03 -0400

    events  crud

---
+++ controllers/events.js
@@ -0,0 +1,40 @@
+const { response } = require("express")
+
+
+
+const getEventos = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'getEventos'
+    })
+}
+
+const crearEvento = (req, res = response) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'crearEvento'
+    })
+}
+
+const actualizarEvento = (req, res = response) => {
+    res.status(200).json({
+        ok: true,
+        msg: 'actualizarEvento'
+    })
+}
+
+const EliminarEvento = (req, res = response) => {
+    res.status(200).json({
+        ok: true,
+        msg: ' eliminarEvento'
+    })
+}
+
+
+
+module.exports = {
+    getEventos,
+    crearEvento,
+    actualizarEvento,
+    EliminarEvento
+}
